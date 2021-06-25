8a85cf62c9e3ac740ad0b0cb7a544cb380f24196
TylerLott <tylerlott0126@gmail.com>
2021-06-25 13:07:46 -0600

    updated chmod on scripts

--- scripts/resume-site-backend-flask-rest-api-redeploy.sh
+++ scripts/resume-site-backend-flask-rest-api-redeploy.sh
@@ -3,5 +3,6 @@
 
 sudo docker pull tylerlott/resume-site-backend-flask-rest-api:prod
 sudo docker stop resume-site-backend-flask-rest-api
-sudo docker system prune -f
+sudo docker image prune -f
+sudo docker container prune -f
 sudo docker run -d --network resume-site-network-prod --name=resume-site-backend-flask-rest-api tylerlott/resume-site-backend-flask-rest-api:prod
--- scripts/resume-site-frontend-react-app-redeploy.sh
+++ scripts/resume-site-frontend-react-app-redeploy.sh
@@ -3,5 +3,6 @@
 
 sudo docker pull tylerlott/resume-site-frontend-react-app:prod
 sudo docker stop resume-site-frontend-react-app
-sudo docker system prune -f
+sudo docker image prune -f
+sudo docker container prune -f
 sudo docker run -d -p 3000:3000 --network resume-site-network-prod --name=resume-site-frontend-react-app tylerlott/resume-site-frontend-react-app:prod
