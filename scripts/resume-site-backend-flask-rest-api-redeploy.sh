12a3c4341423cf5a287659ea198dd4d5ef0a24b9
TylerLott <tylerlott0126@gmail.com>
2021-06-26 00:44:13 -0600

    changed webhook .sh

--- scripts/resume-site-backend-flask-rest-api-redeploy.sh
+++ scripts/resume-site-backend-flask-rest-api-redeploy.sh
@@ -4,7 +4,7 @@
 sudo docker pull tylerlott/resume-site-backend-flask-rest-api:prod
 sudo docker stop resume-site-backend-flask-rest-api
 sudo docker stop resume-site-frontend-react-app
-sudo docker image prune -f
 sudo docker container prune -f
 sudo docker run -d --network resume-site-network-prod --name=resume-site-backend-flask-rest-api tylerlott/resume-site-backend-flask-rest-api:prod
 sudo docker run -d -p 3000:3000 --network resume-site-network-prod --name=resume-site-frontend-react-app tylerlott/resume-site-frontend-react-app:prod
+sudo docker image prune -f
